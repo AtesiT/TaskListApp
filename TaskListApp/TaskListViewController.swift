c3890edbd3f998d73da0b5cd0598076a3c1a2c56
AtesiT <atesit@mail.ru>
2025-12-28 13:28:22 +0300

    MADE: Initialize ViewController without Storyboard was deleted mainstoryboard

---
+++ TaskListApp/TaskListViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class TaskListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
