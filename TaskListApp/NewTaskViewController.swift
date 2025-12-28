25815fc49d42803e5be3eeecdc5ea6a85836bf02
AtesiT <atesit@mail.ru>
2025-12-28 17:14:28 +0300

    MADE: button cancel

--- TaskListApp/NewTaskViewController.swift
+++ TaskListApp/NewTaskViewController.swift
@@ -26,10 +26,28 @@ final class NewTaskViewController: UIViewController {
         return button
     }()
     
+    private lazy var cancelButton: UIButton = {
+        //  Установка атрибутов для кнопки
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        //  Создание кнопки
+        var buttonConfig = UIButton.Configuration.filled()
+        buttonConfig.baseBackgroundColor = .milkRed
+        buttonConfig.attributedTitle = AttributedString("Cancel", attributes: attributes)
+        
+        let button = UIButton(configuration: buttonConfig, primaryAction: UIAction {[unowned self] _ in
+            dismiss(animated: true)
+        })
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        setupSubviews(taskTextField, saveButton)
+        setupSubviews(taskTextField, saveButton, cancelButton)
         setConstraints()
     }
     
@@ -63,6 +81,15 @@ private extension NewTaskViewController {
                 saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20),
                 saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
                 saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+                
+            ]
+        )
+        
+        NSLayoutConstraint.activate(
+            [
+                cancelButton.topAnchor.constraint(equalTo:  saveButton.bottomAnchor, constant: 20),
+                cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+                cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
                 
             ]
         )
